e7815eb7594a29f754d161722a6f7f1192fce854
colmugx <colmugx@qq.com>
2025-04-17 17:40:00 +0800

    feat: implement uninstall command and related utilities

--- platform/os.zig
+++ platform/os.zig
@@ -22,12 +22,51 @@ pub fn create_symlink(
     try fs.symLinkAbsolute(target, symlink, .{});
 }
 
+pub fn deleteDirectoryRecursive(allocator: std.mem.Allocator, dir_path: []const u8) !void {
+    var dir = try fs.openDirAbsolute(dir_path, .{ .iterate = true });
+    defer dir.close();
+
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        const full_entry_path = try fs.path.join(allocator, &.{ dir_path, entry.name });
+        switch (entry.kind) {
+            .sym_link, .file => {
+                try fs.deleteFileAbsolute(full_entry_path);
+            },
+            .directory => {
+                try deleteDirectoryRecursive(allocator, full_entry_path);
+            },
+            else => {},
+        }
+    }
+
+    try fs.deleteDirAbsolute(dir_path);
+}
+
 // export
 export fn zig_create_symlink(target: util.moonbit_bytes_t, symlink: util.moonbit_bytes_t) callconv(.C) void {
     const target_slice = util.moonbitBytesToCStr(target) catch return;
     const symlink_slice = util.moonbitBytesToCStr(symlink) catch return;
 
     create_symlink(target_slice, symlink_slice) catch return;
+}
+
+export fn zig_read_link(symlink: util.moonbit_bytes_t) callconv(.C) util.moonbit_bytes_t {
+    var buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const symlink_slice = util.moonbitBytesToCStr(symlink) catch return null;
+    const path = std.fs.readLinkAbsolute(symlink_slice, &buffer) catch return null;
+
+    return util.cStrToMoonbitBytes(path) catch return null;
+}
+
+export fn zig_remove_dir(dir: util.moonbit_bytes_t) callconv(.C) void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const dir_slice = util.moonbitBytesToCStr(dir) catch return;
+
+    deleteDirectoryRecursive(allocator, dir_slice) catch return;
 }
 
 export fn zig_get_arch() callconv(.C) util.moonbit_bytes_t {
