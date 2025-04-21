419389537e6bceeaa89dbf3994df50b08d48395b
colmugx <colmugx@qq.com>
2025-04-21 13:42:26 +0800

    feat: enhance build process with target architecture checks and static linking

--- build.zig
+++ build.zig
@@ -16,6 +16,30 @@ pub fn build(b: *std.Build) !void {
         .preferred_optimize_mode = .ReleaseSmall,
     });
 
+    const allowed = blk: {
+        const os = target.result.os.tag;
+        const arch = target.result.cpu.arch;
+        if (
+        // macos arm64
+        (os == .macos and arch == .aarch64) or
+            // macos x86_64
+            (os == .macos and arch == .x86_64) or
+            // windows arm64
+            (os == .windows and arch == .aarch64) or
+            // windows x86_64
+            (os == .windows and arch == .x86_64) or
+            // linux (不限架构)
+            (os == .linux))
+        {
+            break :blk true;
+        }
+        break :blk false;
+    };
+    if (!allowed) {
+        std.debug.print("Unsupported target: {s} {s}\n", .{ @tagName(target.result.os.tag), @tagName(target.result.cpu.arch) });
+        return error.UnsupportedTarget;
+    }
+
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("platform/root.zig"),
         .target = target,
@@ -44,7 +68,7 @@ pub fn build(b: *std.Build) !void {
     const lib = b.addLibrary(.{
         .name = "request",
         .root_module = lib_mod,
-        .linkage = .dynamic,
+        .linkage = .static,
     });
 
     const exe = b.addExecutable(.{
@@ -74,6 +98,11 @@ pub fn build(b: *std.Build) !void {
     exe.addObjectFile(b.path("ntm/target/native/release/build/.mooncakes/moonbitlang/x/sys/internal/ffi/libffi.a"));
 
     exe.linkLibrary(lib);
+
+    // Linux 下需要手动链接 math 库
+    if (target.result.os.tag == .linux) {
+        exe.linkSystemLibrary("m");
+    }
 
     b.installArtifact(exe);
 }
